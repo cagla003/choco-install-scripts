b71bb99cd696b5ea12f79122f7081270987ed249
cagla sarf <116388623+cagla003@users.noreply.github.com>
2024-07-01 16:56:33 +0300

    Update ChocolateyInstall.ps1

--- ChocolateyInstall.ps1
+++ ChocolateyInstall.ps1
@@ -1,3 +1,1 @@
-# ChocolateyInstall.ps1
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://github.com/cagla003/choco/ChocolateyInstall.ps1'))
